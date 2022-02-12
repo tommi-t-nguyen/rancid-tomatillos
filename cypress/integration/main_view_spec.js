d10a9b3959991f380327dd98bb5c6c3f760d85a1
Susanna Carey <susannaopal@gmail.com>
2022-02-12 14:25:06 -0500

    Update all tests to include should be visible testing

--- cypress/integration/main_view_spec.js
+++ cypress/integration/main_view_spec.js
@@ -7,13 +7,30 @@ describe('Main View User Flows', () => {
   it('should be able to visit the url', () => {
     cy.url()
     .should('eq', 'http://localhost:3000/');
+
   });
   
   it('should be able to see a header', () => {
     cy.get('h1')
-    .contains('Rotten Tomatillos');
-  })
-});
+    .contains('Rotten Tomatillos')
+    .should('be.visible');
+  });
+
+  //there are 5 in dummy data? Not sure how to test for this? 
+  it('should display 5 different movie poster options on home page', () => {
+    cy.get('.movies-container')
+    .should('be.visible');
+
+  });
+
+  //This one is broken, need to figure out how to fix! see note below on clicking movie with another intercept?
+  // it('should be able to click a movie poster from on the main page and be taken to single movie details', () => { 
+  //   cy.get('.movie-poster-img')    
+  //   .contains('movie-details-container)
+  //   .click();
+  //   });
+
+  });
 
 //TEST URL in the describe block
 // .url
@@ -21,3 +38,9 @@ describe('Main View User Flows', () => {
 //it should display movies details when a movie is clicked & intercept the URL for that movie (heroku app link url) and replace it with indvidual url
 //test clicking  
 //cypress commands 
+
+  //test for click intercept ??
+  //how many visible?
+  //status code
+  //working or an error
+  //test for movie card button to go to a single view 
--- cypress/integration/single_movie_spec.js
+++ cypress/integration/single_movie_spec.js
@@ -5,24 +5,27 @@
      .visit('http://localhost:3000/movies/340102');
   })
 
-  it('should be able to visit the url for a single movie', () => {
+  it('should be able to visit the url for a single movie and corrent movie details be returned', () => {
     cy.url()
     .should('eq', 'http://localhost:3000/movies/340102');
   });
   
   it('should see a title upon page load', () => {
     cy.get('.title')
-    .contains('The New Mutants');
+    .contains('The New Mutants')
+    .should('be.visible');
   });
 
   it('should see a tagline for each single movie if available in data set', () => {
     cy.get('.tagline')
-    .contains('It\'s time to face your demons');
+    .contains('It\'s time to face your demons')
+    .should('be.visible');
   });
 
   it('should see an overview for each single movie viewed', () => {
     cy.get('.overview')
-    .contains('Five young mutants, just discovering their abilities while held in a secret facility against their will, fight to escape their past sins and save themselves.');
+    .contains('Five young mutants, just discovering their abilities while held in a secret facility against their will, fight to escape their past sins and save themselves.')
+    .should('be.visible');
   });
 
   //IS THIS ONE DONE CORRECTLY?
@@ -34,28 +37,44 @@
   
   it('should see a starred rating for each movie viewed', () => {
     cy.get('.rating')
-    .contains(4);
+    .contains(4)
+    .should('be.visible');
   }); 
 
   it('should see a single movie run time', () => {
     cy.get('.runtime')
     .contains(94)
+    .should('be.visible');
   });
   
   it('should see a release date for a single movie', () => {
     cy.get('.release-date')
     .contains("2020-08-26")
+    .should('be.visible');
   });
 
+  it('should be able to click the return home text box with the arrow icon to be returned to the main page', () => { 
+    cy.get('.return-home')
+    .contains('Home ↩')
+    .should('be.visible')
+    .click();
+    });
+    
+  //DO I need to have these two bottom tests?
   it('should see all details for image, rating, runtime, and release date on left side of a single movie details view', () => {
-    cy.get('.left-details');
+    cy.get('.left-details')
+    // .contains();
   });
 
   it('should see all details for title, tagline, and overview on right side of a single movie details card', () => {
-    cy.get('.right-details');
+    cy.get('.right-details')
+    // .contains()
   });
 
+
+//Think still need these: 
   //test for click intercept ??
-  //test for back button click to return home
+  //status code
+  //working or an error
 
 });
