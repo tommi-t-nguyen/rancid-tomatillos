e1d896a5d42574eb44bde2d55f2e7f9b1daa127c
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-02-12 19:36:06 -0500

    Update hero

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,7 +5,6 @@ import { NavLink } from 'react-router-dom';
 const Card = ({id, poster_path, title, switchView}) => {
   return (
     <div className='card' id={id}>
-      <h3>{title}</h3>
       <NavLink to={`/movies/${id}`}>
       <img className='movie-poster-img' src={poster_path} alt='movie poster'/>
       </NavLink>
