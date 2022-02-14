4864549b63749bafcb528ca2aa621e9de4dbaf5b
Susanna Carey <susannaopal@gmail.com>
2022-02-13 20:34:20 -0500

    Add error handling tests to check message with and without wifi

---
+++ cypress/integration/error_spec.js
@@ -0,0 +1,15 @@
+describe('Error Flow', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {statusCode: 500})
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should show an error message if a 500 error occurs', () => {
+    cy.contains('Sorry we\'re having techincal difficulty. Please try again later.');
+  });
+
+  it('should show an error if a 404 error occurs', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {statusCode: 404})
+    cy.visit('http://localhost:3000/');
+  })
+})
