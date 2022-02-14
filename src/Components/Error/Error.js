491b0dd769c2e2eec0fa024908991ef78d786d92
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-02-14 17:13:49 -0500

    Add error page

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import "./Error.css";
+import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className="error">
+				<h1>We're Experiencing Technical Difficulties</h1>
+				<p>Sorry for any inconvenience. Please try again.</p>
+				<p><Link to="/">Back to Home</Link></p>
+			</div>
   )
 }
 
