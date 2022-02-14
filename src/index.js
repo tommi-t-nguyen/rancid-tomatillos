bbb556b53496cfc0dee97a533ce65cda93315500
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-02-13 21:29:51 -0500

    Update front page css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
