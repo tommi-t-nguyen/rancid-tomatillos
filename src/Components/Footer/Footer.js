80c2d6fd6df50aa851dff49bfd3346f9694cac7c
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-02-14 20:12:18 -0500

    Add footer and links to github

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import { Link } from 
+import React from 'react';
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>MyComponent</div>
+    <div className='footer'>
+      <h4>This project was created by <a target="_blank" rel="noreferrer" href="https://github.com/tommi-t-nguyen">
+      Thomas Nguyen</a> & <a target="_blank" rel="noreferrer" href="https://github.com/susannaopal">
+      Susanna Carey</a></h4>
+    </div>
   )
 }
 
